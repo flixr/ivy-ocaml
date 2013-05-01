e3c97b2d76c344c876de23f175ac639fafb39e8e
hattenberger <hattenberger@b9a2a475-3c1e-4ae0-83c4-c96c2d81b069>
2013-05-01 22:48:44 +0000

    remove accidental trunk dir
    git-svn-id: http://svn.tls.cena.fr/svn/ivy/ivy-ocaml/trunk@3580 b9a2a475-3c1e-4ae0-83c4-c96c2d81b069

--- trunk/examples/glibivyprobe.ml
+++
@@ -1,9 +0,0 @@
-(* $Id$ *)
-
-let _ =
-  Ivyprobe.init ();
-  try
-    ignore (GlibIvy.set_up_channel Unix.stdin Ivy.stop (fun _ -> Ivyprobe.read stdin));
-    GlibIvy.main ()
-  with
-    End_of_file -> Ivy.stop ()
