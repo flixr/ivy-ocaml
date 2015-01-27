091580a6a6cf907b571d33762853135d16ba15c3
Felix Ruess <felix.ruess@gmail.com>
2015-01-27 19:26:04 +0100

    declare strings with CAMLlocal so they can be garbage collected

--- civy.c
+++ civy.c
@@ -41,24 +41,28 @@ value ivy_init(value vappName, value vready, value closure_name)
   char * ready = malloc(strlen(String_val(vready))+1); /* Memory leak */
   strcpy(ready, String_val(vready));
   IvyInit(appName, ready, app_cb, (void*)closure, 0, 0); /* When the "die callback" is called ??? */
-  CAMLreturn(Val_unit);;
+  CAMLreturn (Val_unit);;
 }
 
 value ivy_start(value bus)
 {
   CAMLparam1 (bus);
   IvyStart(String_val(bus));
-  CAMLreturn(Val_unit);;
+  CAMLreturn (Val_unit);;
 }
 
 void ClosureCallback(IvyClientPtr app, void *closure, int argc, char **argv)
 {
+  CAMLparam0 ();
+  CAMLlocal1 (data);
   char* t[argc+1];
   int i;
   /* Copie de argv dans t avec ajout d'un pointeur nul a la fin */
   for(i=0; i < argc; i++) t[i] = argv[i];
   t[argc] = (char*)0L;
-  callback2(*(value*)closure, Val_long(app), copy_string_array((char const **)t));
+  data = copy_string_array((char const **)t);
+  callback2(*(value*)closure, Val_long(app), data);
+  CAMLreturn0;
 }
 
 value ivy_bindMsg(value cb_name, value regexp)
@@ -73,18 +77,22 @@ value ivy_unbindMsg(value id)
 {
   CAMLparam1 (id);
   IvyUnbindMsg((MsgRcvPtr)Long_val(id));
-  CAMLreturn(Val_unit);;
+  CAMLreturn (Val_unit);
 }
 
 value ivy_name_of_client(value c)
 {
   CAMLparam1 (c);
-  CAMLreturn (copy_string(IvyGetApplicationName((IvyClientPtr)Long_val(c))));
+  CAMLlocal1 (name);
+  name = copy_string(IvyGetApplicationName((IvyClientPtr)Long_val(c)));
+  CAMLreturn (name);
 }
 value ivy_host_of_client(value c)
 {
   CAMLparam1 (c);
-  CAMLreturn (copy_string(IvyGetApplicationHost((IvyClientPtr)Long_val(c))));
+  CAMLlocal1 (host);
+  host = copy_string(IvyGetApplicationHost((IvyClientPtr)Long_val(c)));
+  CAMLreturn (host);
 }
 
 void cb_delete_channel(void *delete_read)
